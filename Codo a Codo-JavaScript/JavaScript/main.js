8ee43338c0a39f4ea39904afc9362e36a86304a3
Ankyara <romina.doval@gmail.com>
2022-05-30 14:45:37 -0300

    hasta clase de repaso

--- Codo a Codo-JavaScript/JavaScript/main.js
+++ Codo a Codo-JavaScript/JavaScript/main.js
@@ -424,20 +424,20 @@ personas[2]
 
  /* -------------------------------------------------------------------------- */
 
- let persona4 = {
-    nombre: "Ana",
-    apellido: "Salcedo",
-    edad: 35,
-    correo: "ana.salcedo@gmail.com",
-    //hobbie: "tenis"
-    hobbies: ["tenis", "ajedrez", "guitarra"],
+//  let persona4 = {
+//     nombre: "Ana",
+//     apellido: "Salcedo",
+//     edad: 35,
+//     correo: "ana.salcedo@gmail.com",
+//     //hobbie: "tenis"
+//     hobbies: ["tenis", "ajedrez", "guitarra"],
 
-    mascota: {
-        nombre: "Junior",
-        tipoMascota: "Perro",
-        raza: "Caniche"
-    }
-}
+//     mascota: {
+//         nombre: "Junior",
+//         tipoMascota: "Perro",
+//         raza: "Caniche"
+//     }
+// }
 
 /* consola
 
@@ -447,8 +447,251 @@ persona4.hobbies
 persona4.mascota
 */
 
+/* -------------------------------------------------------------------------- */
+/*                              Ejemplos OBJETOS                              */
+/* -------------------------------------------------------------------------- */
+
+// let persona5 = {
+//     nombre:"Juan",
+//     apellido: "Perez",
+//     edad: 25,
+//     correo: "correo@gmail.com",
+//     hobbies: ["Tenis","Ajedrez","Piano"],
+//     mascotas: [{nombre: "Junior", tipo:"Perro", peso:"4 kilos"},
+//                 {nombre: "Cleta", tipo:"Tortuga", peso:"6 kilos"}
+//               ]
+// }
+
+
+// let persona6 = {
+//     nombre:"Silvia",
+//     apellido: "Gonzalez",
+//     edad: 58,
+//     correo: "silvia@gmail.com"
+// }
+
+
+// let product7 = {
+//     id: 1,
+//     nombreProducto: "PeineBlando",
+//     descripcion: "Peine para Mascotas",
+//     precio: 700
+// }
+
+
+// let productos = [
+//     {id: 1,
+//     nombre:"Peine", 
+//     precio: 500},
+//     {id: 2,
+//     nombre:"Cepillo",
+//     precio: 700}
+// ]
+
 
 /* -------------------------------------------------------------------------- */
+/*                                  FUNCIONES                                 */
+/* -------------------------------------------------------------------------- */
+
+// function sumar() {
+//     console.log(5+6)
+// };
+//resultado 11 Undefined
+
+// function sumar() {
+//     return (5+6)
+// };
+
+// consola -> sumar() 
+
+
+// function sumar (num1, num2) {
+//     return (num1 + num2)
+// };
+
+// consola -> sumar(15,25)
+
+/* -------------------------------------------------------------------------- */
+
+function saludar (nombre) {
+    return "Hola " + nombre;
+}
+
+// consola -> saludar("Romina")
+
+
+//* -------------------------- Función Tradicional -------------------------- */
+
+// function saludar(nombre){
+//     return "Hola que tal " + nombre;
+// }
+
+// consola -> saludar("Ana")
+
+
+//* ---------------------------- Función Anónima ---------------------------- */
+//variable de tipo function - La function en si misma no lleva ningun nombre
+
+// let saludar1 = function(nombre){
+//     return "Hola como estas " + nombre;
+// }
+
+// consola -> saludar1("nombre")
+
+// let suma = function(num1, num2){
+//     return(num1+num2);
+// }
+
+//* ---------------------------- Función Flecha ----------------------------- */
+
+// let saludar2 = (nombre) => {
+//     return "Hola que tal " + nombre;
+// }
+
+// let saludar3 = nombre => "Hola que tal " + nombre; // funcion
+
+
+/* ------- Función de flecha que reciba un numero y devuelva el doble ------- */
+
+// let doble2 = function(num){
+//     return num * 2;
+// }
+
+// let doble = numero => numero * 2; // o -> let doble = numero => numero * numero;
+
+
+ /* -------------------------------------------------------------------------- */
+ /*                        Estructura de control ciclica                       */
+ /* -------------------------------------------------------------------------- */
+
+// for(let i=1; i <= 10; i++){
+//     console.log(saludar(i));    
+// }
+
+/* -------------------------------------------------------------------------- */
+
+// function saludar4(num){
+//     return "Hola que tal " + num;
+// }
+
+// console.log(saludar4(1000));
+
+
+// for(let i=1; i <= 10; i++){
+//     console.log(saludar4(i));    
+// }
+
+//* ------------------------------------------------------------------------- */
+
+// let num3 = 9;
+
+// let suma1 = function(num1, num2){
+//     return(num1+num2+num3);
+// }
+
+/* -------------------------------------------------------------------------- */
+/*                             AREGLOS Y FUNCIONES                            */
+/* -------------------------------------------------------------------------- */
+//* las funciones a veces forman parte de una array
+
+// let numeros = [10, 45, 16, 13];
+
+// let doble = numero => numero * 2;
+
+// let dobles = numeros.map(numero => numero * 2); //callback
+// let dobles1 = numeros.map(doble); // callback
+// crea un nuevo array con igual cantidad de elementos (pero cuyos valores son dobles)
+
+
+/* -------------------------------------------------------------------------- */
+/*                   Recorrer un arreglo con un ciclo (for)                   */
+/* -------------------------------------------------------------------------- */
+
+// let nums = [2,4,6];
+
+// for(let i=0; i <= 2; i++) {
+//     console.log(nums[i]);
+// }
+
+// for(let i=0; i < nums.length; i++) {
+//     console.log(nums[i]);
+// }
+
+/* -------------------------------------------------------------------------- */
+
+
+// let nums = [2,4,6];
+// let duplicado = [];
+
+// for (let i = 0; i < nums.length; i++) {
+//     duplicado.push(nums[i] * 2);
+// }
+
+/* -------------------------------------------------------------------------- */
+
+// let nombres = ["Raul","Pedro","Juana"];
+
+// for(let j = 0; j < nombres.length; j = j + 1){
+//     console.log("Hola " + nombres[j]);
+// }
+
+ //* ------------------------- Ejemplo de For...each ------------------------- */
+ 
+// let nombres = ["Raul","Pedro","Juana"];
+
+// nombres.forEach(element => {
+//     console.log(element);
+// });
+
+
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCIONES                                 */
+/* -------------------------------------------------------------------------- */
+
+// function saludarx () {
+//     return ("Hola");
+// }
+// console.log(saludarx())
+
+
+// function saludar(nombre, apellido){
+//     return("Hola " + nombre + " " + apellido);
+// }
+
+/* -------------------------------------------------------------------------- */
+
+function caminar(){
+    return("Caminando");
+}
+console.log(caminar()) // Ejecución de la function (lo que devuelve la funcion)
+
+
+function dormir(){
+    return("Durmiendo");
+}
+
+
+//* una function puede recibir otra función como parámetro y la puede ejecutar adentro
+//* callback?
+
+function accion(funcion){
+    return funcion();
+}
+// consola accion(caminar);
+
+console.log(accion(caminar)); // Funcion
+
+/* -------------------------------------------------------------------------- */
+
+
+
+
+
+
+
+
+
+
 
 
 
