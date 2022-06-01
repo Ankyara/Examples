4c27aa6dc27bddd98fedaa82a4987901a9516148
Ankyara <romina.doval@gmail.com>
2022-06-01 18:45:24 -0300

    tuve que intalar git de nuevo

--- Codo a Codo-JavaScript/JavaScript/main2.js
+++ Codo a Codo-JavaScript/JavaScript/main2.js
@@ -134,29 +134,104 @@ let regex;
 
 /* --------------- Ejemplo2 (lo mismo pero de distinta forma) --------------- */
 
-form.addEventListener("submit", (evento)=>{
+// form.addEventListener("submit", (evento)=>{
 
-    evento.preventDefault();
+//     evento.preventDefault();
 
-    regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/gi;
+//     regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/gi;
 
-    // if(nombre.value.includes("@")){
-    //     console.log("Enviando el Formulario");
-    // } else {
-    //     console.log("Dato inválido");
-    // }
+//     // if(nombre.value.includes("@")){
+//     //     console.log("Enviando el Formulario");
+//     // } else {
+//     //     console.log("Dato inválido");
+//     // }
 
-    let valor = nombre.value;
+//     let valor = nombre.value;
 
-    if(regex.test(valor)){
-        console.log("Enviando el Formulario");
-    } else {
-        console.log("Dato inválido");
-    }
-} )
+//     if(regex.test(valor)){
+//         console.log("Enviando el Formulario");
+//     } else {
+//         console.log("Dato inválido");
+//     }
+// } )
 
 
 // evento / e / ev
 // expresion regular -> de búsqueda -> / /
 
+
+
 /* -------------------------------------------------------------------------- */
+/*                     ASINCRONÍA - EJECUCIÓN EN 2° PLANO                     */
+/* -------------------------------------------------------------------------- */
+
+console.clear()
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log("Hola")
+
+/* -------------------------------------------------------------------------- */
+// console.log(1)
+// console.log(2)
+
+// window.setTimeout(function() {
+//     console.log("Hola")
+// }, 3000)
+
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+
+
+// el "hola" se ejecuta después de 3 segundos, ejecución en segundo plano
+// el código se sigue ejecutando y después de los 3 seg, se ejecuta el "hola"
+
+/* -------------------------------------------------------------------------- */
+
+// var xhr = new XMLHttpRequest();
+
+// console.log(xhr.readyState);
+
+// xhr.open("get", "archivo.txt");
+// console.log(xhr.readyState);
+
+// xhr.send();
+// console.log(xhr.readyState);
+
+// // setTimeout(() =>{
+// //     console.log("Texto recibido: " + xhr.responseText)
+// // }, 5000);
+
+// xhr.addEventListener("readystatechange", () => {
+//     if(xhr.readyState ==4){
+//         console.log("Texto recibido: " + xhr.responseText);
+//     }
+// })
+
+//open -> que quiero hacer y donde lo busque (dirección)
+// send -> donde lo mando
+
+/* -------------------------------------------------------------------------- */
+
+var xhr = new XMLHttpRequest();
+xhr.open("get", "archivo.txt");
+
+console.log(xhr.readyState);
+
+xhr.addEventListener("readystatechange", () => {
+    console.log(xhr.readyState);
+    if(xhr.readyState ==4){
+        console.log("Texto recibido: " + xhr.responseText);
+    }
+})
+
+xhr.send();
+
+/* -------------------------------------------------------------------------- */
+
